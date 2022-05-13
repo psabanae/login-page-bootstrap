9d249d7cc8525561c72c79db1870ce48bb96148f
Paulo Sabanae <psabanae3@gmail.com>
2022-05-13 02:47:09 -0400

    Fixed alert

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
-const alertDiv = document.querySelector('.alert');
 const alertButton = document.querySelector('.btn-alert');
 const inputUser = document.querySelector('#usuario');
 const inputPw = document.querySelector('#senha');
 
+const innerAlertDiv = `<div class="alert alert-success alert-dismissible fade show" role="alert">
+<i class="bi bi-check-circle-fill"></i> Login Efetuado!
+<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>`
+
 alertButton.addEventListener('click', () => {
-    if(inputUser.value.length && inputPw.value.length){
-        alertDiv.style.display = 'block';
+    const alertDiv = document.querySelector('.alert');
+
+    if(!inputUser.value || !inputPw.value) {
+        return false;
+    }
+
+    if(!alertDiv) {
+        const div = document.createElement('div');
+        div.innerHTML += innerAlertDiv;
+        document.body.prepend(div);
     }
 })
